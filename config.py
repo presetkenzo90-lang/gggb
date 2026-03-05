49e97d55238b50ba1b7f48b7c2e1f6fff649ee6a
presetkenzo90-lang <presetkenzo90@gmail.com>
2026-03-05 17:39:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+# 🔥 GET FROM @BotFather
+BOT_TOKEN = "8672722368:AAFdDVyYOuz7Ebua0Ffv-daNI3m3D2_tfIk"
+
+# 🔥 YOUR ID FROM @userinfobot
+AUTHORIZED_USERS = [7271109983]  
+
+TARGET_LINKS = {
+    "stripe_checkout": "https://checkout.stripe.com/api/v1/payment_intents",
+    "xsolla": "https://api.xsolla.com/v1/payments",
+    "stripe_js": "https://js.stripe.com/v3/"
+}
+
+LIVE_BINS = {
+    "visa": "4532015112830366",
+    "mc": "5555555555554444",
+    "amex": "378282246310005",
+    "stripe_live": "4242424242424242"
+}
